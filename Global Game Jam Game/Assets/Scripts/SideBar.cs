5b3c68860a4bae462838215d00f0a20b8a5bfc1f
RoyalRB <Richardbenthem1423@gmail.com>
2020-01-31 22:26:06 +0100

    Pop up menu thing
    I don't like the scrolling thingy, it's hard :/

---
+++ Global Game Jam Game/Assets/Scripts/SideBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideBar : MonoBehaviour
+{
+    private void Start()
+    {
+        MoveInMenu();
+    }
+
+    public void MoveInMenu()
+    {
+        gameObject.SetActive(true);
+    }
+}
