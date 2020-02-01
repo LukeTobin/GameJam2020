2825de5e64705b225cf18d28566e1d755beae8cd
RoyalRB <Richardbenthem1423@gmail.com>
2020-02-01 19:06:06 +0100

    Egh
    Egh

---
+++ Global Game Jam Game/Assets/Scripts/ReadyPlacement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadyPlacement : MonoBehaviour
+{
+
+    [SerializeField] GameObject plant;
+    public GameObject storing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnMouseDown()
+    {
+       //storing.
+    }
+}
